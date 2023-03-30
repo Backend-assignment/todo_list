8251a19c36320f5b13ca0e5b2fcc5e3114b1f5dd
ZARIFMAC <uzbzarif@gmail.com>
2023-03-30 09:46:43 +0500

    templates

--- core/urls.py
+++ core/urls.py
@@ -1,11 +1,13 @@
 
 from django.contrib import admin
 from django.urls import path
-from api.views import HomeView
-from api.views import index
+
+from api.views import tasks,home,index
+
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('', HomeView.as_view()),
-    path('index/', index)
+    path('tasks/', tasks),
+    path('home/', home),
+    path('index/', index),
 ]
